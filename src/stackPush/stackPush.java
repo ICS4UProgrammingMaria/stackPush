1a3172d1e41ddff751d0f715f1cab40cf60b0fd2
Maria T <maria.temu@ocsbstudent.ca>
2020-03-12 12:05:14 -0400

    Initial commit

---
+++ src/stackPush/stackPush.java
@@ -0,0 +1,15 @@
+package stackPush;
+
+import javax.swing.*;
+
+public class stackPush {
+
+
+    private JTextField txtInteger;
+    private JButton btnValueButton;
+    private JLabel lblInteger;
+
+    public void main(String[] args) {
+
+    }
+}
